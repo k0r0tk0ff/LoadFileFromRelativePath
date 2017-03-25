56de103bc00c640d67de2e88bde26c2e2b9489be
k0r0tk0ff <korotkov34reg@gmail.com>
2017-03-25 10:53:16 +0300

    2

--- README.md
+++ README.md
@@ -3,3 +3,9 @@ work in study project with Petr Arsentev
 http://job4j.ru/
 
 video - https://www.youtube.com/watch?v=kQ-475fKKZ0
+
+
+Need to read -
+page "How do I add resources to my JAR?"
+http://maven.apache.org/guides/getting-started/index.html#How_do_I_add_resources_to_my_JAR
+
--- src/main/java/ru/k0r0tk0ff/Service/Settings.java
+++ src/main/java/ru/k0r0tk0ff/Service/Settings.java
@@ -1,33 +1,27 @@
-package ru.k0r0tk0ff.Service;
+package ru.k0r0tk0ff.service;
 
-import java.io.IOException;
 import java.io.InputStream;
 import java.util.Properties;
-import java.util.StringJoiner;
 
 /**
- * Created by user on 3/24/2017.
- * @author Petr Arsentev
+ * //TODO add comments.
+ *
+ * @author Petr Arsentev (parsentev@yandex.ru)
  * @version $Id$
  * @since 0.1
  */
-
 public class Settings {
-
-    /**
-    * Class Properties use to bind "key" and "value"
-    */
-    public final Properties properties = new Properties();
+    public final Properties prs = new Properties();
 
     public void load(InputStream io) {
         try {
-            this.properties.load(io);
-        } catch (Exception error) {
-            error.printStackTrace();
+            this.prs.load(io);
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 
     public String getValue(String key) {
-        return this.properties.getProperty(key);
+        return this.prs.getProperty(key);
     }
 }
